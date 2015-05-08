3732d5c313e0d18d01423223adb912d9cf966107
NikKozh <nikkozh@yandex.ru>
2015-05-09 02:40:07 +0300

    Update Polynomial.cpp

--- DMColloquium/Polynomial.cpp
+++ DMColloquium/Polynomial.cpp
@@ -7,3 +7,24 @@
 //
 
 #include "Polynomial.h"
+
+// Задача P-2, группа: Кожевников Никита, Ташланов Андрей
+Polynomial Polynomial::SUB_PP_P(Polynomial SecondPoly)
+{
+	int i;
+
+	if (coefficients.size() >= SecondPoly.coefficients.size()) // Если первый многочлен больше второго или равен ему
+	{
+		for (i = 0; i < SecondPoly.coefficients.size(); i++)
+			coefficients[i] = coefficients[i].SUB_QQ_Q(SecondPoly.coefficients[i]);
+	}
+	else
+	{
+		for (i = 0; i < coefficients.size(); i++)
+			coefficients[i] = coefficients[i].SUB_QQ_Q(SecondPoly.coefficients[i]);
+		for (RationalNumber null; i < SecondPoly.coefficients.size(); i++) // Создаётся объект null, подразумевается, что он нулевой. Из него вычитается коэффициенты первого многочлена
+			coefficients[i] = null.SUB_QQ_Q(SecondPoly.coefficients[i]);
+	}
+
+	return *this;
+}
