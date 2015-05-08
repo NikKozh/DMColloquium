22065fec1f59eed0aca89e707f7af0f169461dc0
NikKozh <nikkozh@yandex.ru>
2015-05-09 02:41:01 +0300

    Update NaturalNumber.h

--- DMColloquium/NaturalNumber.h
+++ DMColloquium/NaturalNumber.h
@@ -6,21 +6,13 @@
 //  Copyright (c) 2015 Ilya Kulebyakin. All rights reserved.
 //
 
-#ifndef __DMColloquium__NaturalNumber__
-#define __DMColloquium__NaturalNumber__
-
 #include <vector>
-
-#endif /* defined(__DMColloquium__NaturalNumber__) */
 
 class NaturalNumber {
 protected:
-    // you must only use first 9 digits of each int.
-    // So, if a number is: 12345678901234 than
-    // digitBlocks[1] = 12345
-    // digitBlocks[0] = 678901234
-    std::vector<unsigned int> digitBlocks;
+   std::vector<unsigned int> digitBlocks;
 public:
-    NaturalNumber greatestCommonFactor(NaturalNumber secondNumber);
-    // put declarations-ONLY for each method below
+    NaturalNumber ADD_1N_N();
 };
+
+#endif /* defined(__DMColloquium__NaturalNumber__) */
