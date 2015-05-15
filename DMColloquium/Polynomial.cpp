5c58da7ec67534c63188d4aafaeaf25672e84994
NikKozh <nikkozh@yandex.ru>
2015-05-15 22:17:36 +0300

    Update Polynomial.cpp

--- DMColloquium/Polynomial.cpp
+++ DMColloquium/Polynomial.cpp
@@ -7,24 +7,29 @@
 //
 
 #include "Polynomial.h"
+using namespace std;
+Polynomial::Polynomial() {
+    coefficients = vector<RationalNumber>(1);
+}
 
 // Задача P-2, группа: Кожевников Никита, Ташланов Андрей
 Polynomial Polynomial::SUB_PP_P(Polynomial SecondPoly)
 {
+	Polynomial result = *this;
 	unsigned int i;
 
-	if (coefficients.size() >= SecondPoly.coefficients.size()) // Если первый многочлен больше второго или равен ему
+	if (result.coefficients.size() >= SecondPoly.coefficients.size()) // Если первый многочлен больше второго или равен ему
 	{
 		for (i = 0; i < SecondPoly.coefficients.size(); i++)
-			coefficients[i] = coefficients[i].SUB_QQ_Q(SecondPoly.coefficients[i]);
+			result.coefficients[i] = result.coefficients[i].SUB_QQ_Q(SecondPoly.coefficients[i]);
 	}
 	else
 	{
-		for (i = 0; i < coefficients.size(); i++)
-			coefficients[i] = coefficients[i].SUB_QQ_Q(SecondPoly.coefficients[i]);
+		for (i = 0; i < result.coefficients.size(); i++)
+			result.coefficients[i] = result.coefficients[i].SUB_QQ_Q(SecondPoly.coefficients[i]);
 		for (RationalNumber null; i < SecondPoly.coefficients.size(); i++) // Создаётся объект null рационального типа (дробь 0/1). Из него вычитаются коэффициенты второго многочлена в том случае, если первый многочлен оказался меньше и его дальнейшие коэффициенты, из которых нужно вычитать, отсутствуют.
-			coefficients[i] = null.SUB_QQ_Q(SecondPoly.coefficients[i]);
+			result.coefficients[i] = null.SUB_QQ_Q(SecondPoly.coefficients[i]);
 	}
 
-	return *this;
+	return result;
 }
